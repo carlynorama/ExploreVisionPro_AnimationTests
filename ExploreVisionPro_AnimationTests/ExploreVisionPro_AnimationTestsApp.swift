5249f5dce63b86be142e69d0cb13c113d004f06a
carlynorama <carlynorama@gmail.com>
2023-10-10 09:46:56 -0700

    Initial Commit

---
+++ ExploreVisionPro_AnimationTests/ExploreVisionPro_AnimationTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExploreVisionPro_AnimationTestsApp.swift
+//  ExploreVisionPro_AnimationTests
+//
+//  Created by Carlyn Maw on 10/10/23.
+//
+
+import SwiftUI
+
+@main
+struct ExploreVisionPro_AnimationTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}

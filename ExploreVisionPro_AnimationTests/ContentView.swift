3a07fcef6462a8f247b0e2f55fb287ed78d41007
carlynorama <carlynorama@gmail.com>
2023-10-10 11:00:52 -0700

    first test

--- ExploreVisionPro_AnimationTests/ContentView.swift
+++ ExploreVisionPro_AnimationTests/ContentView.swift
@@ -15,6 +15,13 @@ struct ContentView: View {
 
     var body: some View {
         VStack {
+            Model3D(named: "cube_purple_autoplay", bundle: realityKitContentBundle)
+            RealityView { content in
+                if let cube = try? await Entity(named: "cube_purple_autoplay", in: realityKitContentBundle) {
+                    print(cube.components)
+                    content.add(cube)
+                }
+            }
             RealityView { content in
                 // Add the initial RealityKit content
                 if let scene = try? await Entity(named: "Scene", in: realityKitContentBundle) {
@@ -23,9 +30,19 @@ struct ContentView: View {
             } update: { content in
                 // Update the RealityKit content when SwiftUI state changes
                 if let scene = content.entities.first {
+                    
+                    if enlarge {
+                        for animation in scene.availableAnimations {
+                            scene.playAnimation(animation.repeat())
+                        }
+                    } else {
+                        scene.stopAllAnimations()
+                    }
                     let uniformScale: Float = enlarge ? 1.4 : 1.0
                     scene.transform.scale = [uniformScale, uniformScale, uniformScale]
                 }
+                
+               
             }
             .gesture(TapGesture().targetedToAnyEntity().onEnded { _ in
                 enlarge.toggle()
